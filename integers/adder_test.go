180cf1077902ba9c25bbab98f31f34b117b5a114
sezalverma <sezal.verma@nagarro.com>
2022-11-22 22:26:19 +0530

    Tests on Integers & Example test func

---
+++ integers/adder_test.go
@@ -0,0 +1,26 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	sum := Add(3, 4)
+	expected := 7
+	assertCorrectMessage(t, sum, expected)
+}
+
+func ExampleAdd() {
+	// This test will be compiled & not executed if output comment is removed
+	sum := Add(7, 9)
+	fmt.Println(sum)
+	// Output: 16
+}
+
+func assertCorrectMessage(t testing.TB, sum, expected int) {
+	t.Helper()
+	if sum != expected {
+		t.Errorf("Got %d but expected %d", sum, expected)
+	}
+}
