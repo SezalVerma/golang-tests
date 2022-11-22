7a04d2ea99735d743840bb9505a802cf3f84a341
sezalverma <sezal.verma@nagarro.com>
2022-11-22 16:08:00 +0530

    Added different Hello msg for different languages & its tests

--- hello.go
+++ hello.go
@@ -4,16 +4,41 @@ import (
 	"fmt"
 )
 
+const spanish = "Spanish"
+const french = "French"
+const hindi = "Hindi"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const hindiHelloPrefix = "Namaste, "
 
-func Hello(name string) string {
+// private method - 'g'
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case hindi:
+		prefix = hindiHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+// public method - 'H'
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	prefix := greetingPrefix(language)
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Sezal"))
-	fmt.Println(Hello(""))
+	fmt.Println(Hello("Sezal", "English"))
+	fmt.Println(Hello("", ""))
 }
